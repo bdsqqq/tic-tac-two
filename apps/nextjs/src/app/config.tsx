215b7fb070f5de08cdef95a4f12eada89013456a
Igor Bedesqui <igorbedesqui@gmail.com>
2023-10-21 00:14:26 +0100

    feat(config): update site metadata

--- apps/nextjs/src/app/config.tsx
+++ apps/nextjs/src/app/config.tsx
@@ -1,6 +1,7 @@
 export const siteConfig = {
-  name: 'haxiom',
-  description: 'Modern Axiom UI clone',
-  github: 'https://github.com/bdsqqq/haxiom',
+  name: 'Tic-tac-two',
+  description:
+    'The greatest sequel know to mankind, tic-tac-two improves on all aspects of the classic game of tic-tac-toe. Implemented perfectly 100% bug-free  ',
+  github: 'https://github.com/bdsqqq/tic-tac-two',
   twitter: 'https://twitter.com/bedesqui',
 };
