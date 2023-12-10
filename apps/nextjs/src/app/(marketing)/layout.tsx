d94cffffe44f766a1ff00a1e54a632df472d1455
Igor Bedesqui <igorbedesqui@gmail.com>
2023-12-10 12:56:36 -0300

    chore: comment unused code

--- apps/nextjs/src/app/(marketing)/layout.tsx
+++ apps/nextjs/src/app/(marketing)/layout.tsx
@@ -1,8 +1,8 @@
-import { Axiom } from '@haxiom/ui/icons';
+// import { Axiom } from '@haxiom/ui/icons';
 import { cn } from '@haxiom/ui';
 import { MAIN_CONTENT_CLASS, MAX_WIDTH_CLASS } from '~/app/(marketing)/_constants';
-import Link from 'next/link';
-import { Button } from '@haxiom/ui/button';
+// import Link from 'next/link';
+// import { Button } from '@haxiom/ui/button';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,36 +13,36 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   );
 }
 
-const NavigationBar = () => {
-  return (
-    <nav className="sticky top-0 bg border-b">
-      <div className={cn('flex justify-between h-12', MAX_WIDTH_CLASS)}>
-        <div className="flex items-center h-full">
-          <Link
-            href={'/'}
-            className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-4 block flex items-center"
-          >
-            <Axiom className="h-6 w-6" />
-          </Link>
-          <Link
-            href={'/igor'}
-            className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-4 block flex items-center"
-          >
-            Igor's UI Components
-          </Link>
-          <Link
-            href={'/ov'}
-            className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-4 block flex items-center"
-          >
-            Ollie Explorations
-          </Link>
-        </div>
-        <div className="flex items-center h-full">
-          <Button asChild>
-            <Link href={'/datasets'}>Login</Link>
-          </Button>
-        </div>
-      </div>
-    </nav>
-  );
-};
+// const NavigationBar = () => {
+//   return (
+//     <nav className="sticky top-0 bg border-b">
+//       <div className={cn('flex justify-between h-12', MAX_WIDTH_CLASS)}>
+//         <div className="flex items-center h-full">
+//           <Link
+//             href={'/'}
+//             className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-4 block flex items-center"
+//           >
+//             <Axiom className="h-6 w-6" />
+//           </Link>
+//           <Link
+//             href={'/igor'}
+//             className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-4 block flex items-center"
+//           >
+//             Igor's UI Components
+//           </Link>
+//           <Link
+//             href={'/ov'}
+//             className="hover:bg-element-hover focus:bg-element-hover focus-visible:ring-offset-0 h-full px-4 block flex items-center"
+//           >
+//             Ollie Explorations
+//           </Link>
+//         </div>
+//         <div className="flex items-center h-full">
+//           <Button asChild>
+//             <Link href={'/datasets'}>Login</Link>
+//           </Button>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
